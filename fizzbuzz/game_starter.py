79583299c51e489068e05ce32a2a15ef192d7e4a
emp7yhead <artyomkropp@gmail.com>
2022-05-18 21:11:35 +0900

    Fix: move constants to print

--- fizzbuzz/game_starter.py
+++ fizzbuzz/game_starter.py
@@ -4,10 +4,6 @@ from typing import Callable, Union
 
 import prompt
 
-GREETING_MSG = 'Welcome to Fizz Buzz!'
-RULES_MSG = 'Submit a number and get an answer!'
-ASK_NUMBER_MSG = 'Number: '
-
 
 def start_game(game: Callable[[int], Union[str, int]]) -> None:
     """Start a game.
@@ -15,10 +11,10 @@ def start_game(game: Callable[[int], Union[str, int]]) -> None:
     Args:
         game: function that will be sented in infinite loop.
     """
-    print(GREETING_MSG)
-    print(RULES_MSG)
+    print('Welcome to Fizz Buzz!')
+    print('Submit a number and get an answer!')
     while True:
-        puzzled_number = prompt.string(ASK_NUMBER_MSG)
+        puzzled_number = prompt.string('Number: ')
         try:
             final_result = game(int(puzzled_number))
         except ValueError:
