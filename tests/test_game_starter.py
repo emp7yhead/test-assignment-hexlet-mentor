5ee55fc94ed0e0a86c1c770a2efa903fcdfd6746
emp7yhead <artyomkropp@gmail.com>
2022-05-19 17:32:59 +0900

    Update: test_game_starter.py

--- tests/test_game_starter.py
+++ tests/test_game_starter.py
@@ -1,17 +1,15 @@
-"""Test for game_starter.py"""
+"""Test for game_starter.py."""
 import pytest
 
 from fizzbuzz.game_starter import start_game
 
 
-@pytest.fixture
-def game_fixture():
-    pass
+def test_start_game_str_input(monkeypatch):
+    """Test string input.
 
-
-def test_start_game_str_input(monkeypatch, game_fixture):
-    """Test input with string input."""
+    Args:
+        monkeypatch: using for monkeypatching input.
+    """
     monkeypatch.setattr('builtins.input', lambda _: 'hey')
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        start_game(game_fixture)
-    assert pytest_wrapped_e.type == SystemExit
+    with pytest.raises(SystemExit):
+        start_game('random_string_to_trigger_test')
